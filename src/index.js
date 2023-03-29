14908f54224b72bf7047301c674c830a480d145b
hamriyat <vladlahman@gmail.com>
2023-03-29 20:16:08 +0300

    first commit

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import './style.scss';
+
+const menuBtn = document.querySelector('.header__burger');
+const menuBody  = document.querySelector('.menu');
+
+if (menuBtn && menuBody) {
+   menuBtn.addEventListener('click', function (e) {
+      document.body.classList.toggle('scroll-lock-js');
+      menuBtn.classList.toggle('header__burger--active-js');
+      menuBody.classList.toggle('menu--active-js');
+   })
+}
